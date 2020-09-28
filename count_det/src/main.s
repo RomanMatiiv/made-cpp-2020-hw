1e8a896f7765074598ea22c14732e0101432f6b5
RomanMatiiv <matiiv.r.a@gmail.com>
2020-09-28 03:48:52 +0300

    implement determenant 3x3 calculation

--- count_det/src/main.s
+++ count_det/src/main.s
@@ -1,2 +1,64 @@
-/// Your code goes here...
+.text
+.global count_det
 
+count_det:
+        //a11 a12 a13
+        //a21 a22 a23
+        //a31 a32 a33
+
+        // a11 * a22 * a33
+        ldr r2, [r0, #4 * 0]
+        ldr r3, [r0, #4 * 4]
+        ldr r4, [r0, #4 * 8]
+        mul r2, r2,r3
+        mul r2, r2, r4
+
+        add r1, r2
+
+        // a12 * a23 * a31
+        ldr r2, [r0, #4 * 1]
+        ldr r3, [r0, #4 * 5]
+        ldr r4, [r0, #4 * 6]
+        mul r2, r2,r3
+        mul r2, r2, r4
+
+        add r1, r2
+
+        // a13 * a21 * a32
+        ldr r2, [r0, #4 * 2]
+        ldr r3, [r0, #4 * 3]
+        ldr r4, [r0, #4 * 7]
+        mul r2, r2,r3
+        mul r2, r2, r4
+
+        add r1, r2
+
+        // a13 * a22 * a31
+        ldr r2, [r0, #4 * 2]
+        ldr r3, [r0, #4 * 4]
+        ldr r4, [r0, #4 * 6]
+        mul r2, r2,r3
+        mul r2, r2, r4
+
+        sub r1, r2
+
+        // a12 * a21 * a33
+        ldr r2, [r0, #4 * 1]
+        ldr r3, [r0, #4 * 3]
+        ldr r4, [r0, #4 * 8]
+        mul r2, r2,r3
+        mul r2, r2, r4
+
+        sub r1, r2
+
+        // a11 * a23 * a32
+        ldr r2, [r0, #4 * 0]
+        ldr r3, [r0, #4 * 5]
+        ldr r4, [r0, #4 * 7]
+        mul r2, r2,r3
+        mul r2, r2, r4
+
+        sub r1, r2
+
+        mov r0, r1
+        bx lr
