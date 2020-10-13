6930a5208d3342f20b6d2ac932267a03a32c9cb8
RomanMatiiv <matiiv.r.a@gmail.com>
2020-10-13 08:24:59 +0300

    init

--- matrix/src/matrix.cpp
+++ matrix/src/matrix.cpp
@@ -3,4 +3,51 @@
 using namespace task;
 
 
-// Your code goes here...
+class Matrix
+{
+public:
+
+    Matrix() = default;
+
+    Matrix(size_t rows, size_t cols)
+    {
+
+    }
+//    Matrix(const Matrix& copy);
+//    Matrix& operator=(const Matrix& a);
+//
+//    double& get(size_t row, size_t col);
+//    const double& get(size_t row, size_t col) const;
+//    void set(size_t row, size_t col, const double& value);
+//    void resize(size_t new_rows, size_t new_cols);
+//
+//    /* ??? */ operator[](size_t row);
+//    /* ??? */ operator[](size_t row) const;
+//
+//    Matrix& operator+=(const Matrix& a);
+//    Matrix& operator-=(const Matrix& a);
+//    Matrix& operator*=(const Matrix& a);
+//    Matrix& operator*=(const double& number);
+//
+//    Matrix operator+(const Matrix& a) const;
+//    Matrix operator-(const Matrix& a) const;
+//    Matrix operator*(const Matrix& a) const;
+//    Matrix operator*(const double& a) const;
+//
+//    Matrix operator-() const;
+//    Matrix operator+() const;
+//
+//    double det() const;
+//    void transpose();
+//    Matrix transposed() const;
+//    double trace() const;
+//
+//    std::vector<double> getRow(size_t row);
+//    std::vector<double> getColumn(size_t column);
+//
+//    bool operator==(const Matrix& a) const;
+//    bool operator!=(const Matrix& a) const;
+
+    // Your code goes here...
+
+};
